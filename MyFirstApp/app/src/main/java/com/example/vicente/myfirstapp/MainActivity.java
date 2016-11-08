f0950741f14e1970bceb73d48ced1a6cf80a65b1
Vicente Martínez <vicentemartinezf@gmail.com>
2016-11-08 16:20:37 +0100

    First application

---
+++ MyFirstApp/app/src/main/java/com/example/vicente/myfirstapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.vicente.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String ExMessage = "com.example.vicente.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //Called when the send button is pushed
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(ExMessage, message);
+        startActivity(intent);
+    }
+}
